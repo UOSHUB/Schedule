5076d37fd5a2f903e24c3ec81c5a7cabadcbf57c
Omar Einea <eineao@gmail.com>
2016-02-24 22:09:49 +0400

    Make info grabber a class and improve logic

--- info.py
+++ info.py
@@ -4,102 +4,102 @@
 import mechanize
 from bs4 import BeautifulSoup
 
-# Initializes & returns a Mechanize Browser object
-def init_browser():
-    # Instantiate browser
-    br = mechanize.Browser()
-    # Browser options
-    br.set_handle_equiv(True)
-    br.set_handle_redirect(True)
-    br.set_handle_referer(True)
-    br.set_handle_robots(False)
-    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-    # Return browser object
-    return br
 
-# Returns a Mechanize Browser object after logging in
-def login(sid, pin):
-    # Initialize browser
-    br = init_browser()
-    # Open original UOS UDC url
-    br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_WWWLogin")
-    # Fill up login form and submit
-    br.select_form(nr=0)
-    br["sid"] = sid
-    br["PIN"] = pin
-    br.submit()
-    # Return browser object
-    return br
+# A class to manage information flow
+class Info:
 
-# Returns student's first name
-def grab_username(br):
-    # Enter Personal Information section
-    br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_GenMenu?name=bmenu.P_GenMnu")
-    # Enter Directory Profile page
-    br.open(br.click_link(url="/prod_enUS/bwgkoprf.P_ShowDiroItems"))
-    # Use BeautifulSoup to extract data from raw html
-    soup = BeautifulSoup(br.response().read(), "lxml")
-    # Extract and return student name from soup
-    return soup.find("td", class_="dedefault").string.split()[0]
+    # Initializes a Mechanize Browser object
+    def __init__(self):
+        # Instantiate browser
+        self.br = mechanize.Browser()
+        # Browser options
+        self.br.set_handle_equiv(True)
+        self.br.set_handle_redirect(True)
+        self.br.set_handle_referer(True)
+        self.br.set_handle_robots(False)
+        self.br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-# Returns student's schedule
-def grab_schedule(br):
-    # Enter Student -> Registration section
-    br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu")
-    # Enter Student Detail Schedule page
-    br.open(br.click_link(url="/prod_enUS/bwskfshd.P_CrseSchdDetl"))
-    # Select current semester to be displayed
-    br.select_form(nr=1)
-    br.submit()
-    # Use BeautifulSoup to extract info from raw html
-    soup = BeautifulSoup(br.response().read(), "lxml")
-    # Initialize schedule dictionary that'll hold course details
-    schedule = {}
-    key = None
-    # Loop through tables with datadisplaytable class
-    for table in soup.find_all("table", class_="datadisplaytable"):
-        # If it's the heading table
-        if table.caption.string != "Scheduled Meeting Times":
-            # Split table caption into three parts ['name', 'number', 'section']
-            caption = table.caption.string.split(" - ")
-            # Store array key as course number after removing spaces
-            key = caption[1].replace(' ', '')
-            # Store all table cells into row array
-            row = table.find_all("td", class_="dddefault")
-            # Store courses info as: schedule[number] = [name, section, CRN, prof_name, prof_email, credit_hours]
-            schedule[key] = [[caption[0], caption[2], row[1].string, row[3].a.get("target"),
-                             row[3].a.get("href").split(':')[1], row[5].string.split()[0][0]]]
-    # Return to Student -> Registration section
-    br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu")
-    # Enter Student Detail Schedule page
-    br.open(br.click_link(url="/prod_enUS/uos_dispschd.P_DispCrseSchdSum"))
-    # Use BeautifulSoup to extract info from raw html
-    soup = BeautifulSoup(br.response().read(), "lxml")
-    # Loop through TR tags of tables with datadisplaytable class
-    for table in soup.find_all("table", class_="datadisplaytable")[1].find_all("tr"):
-        # Exclude rows with TH tag or that doesn't have valign attribute
-        if table.th is None and table.has_attr("valign"):
-            # Store all table cells into row array
-            row = table.find_all("td", class_="dddefault")
-            # Try and fix if location info is divided into parts
-            try:
-                # Split and store the location info
-                location = row[6].string.split()
-                # If building is repeated in room info
-                if location[0] in location[1]:
-                    # Store only the actual room info
-                    room = location[1].split('-')[1]
-                else:
-                    # Otherwise store the room info as is
-                    room = location[1]
-            # If this fails for some reason
-            except Exception:
-                # Store the whole location string as is
-                location = [row[6].string]
-                # Set where room should be that room info is within location
-                room = "Check in location"
-            # Add more info to schedule as: [[days in chars], [start/end class time], [building, room]]
-            schedule[row[0].string].extend([list(row[4].string), row[5].string.split(" - "), [location[0], room]])
+    # Login to official UOS UDC
+    def login(self, sid, pin):
+        # Open original UOS UDC url
+        self.br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_WWWLogin")
+        # Fill up login form and submit
+        self.br.select_form(nr=0)
+        self.br["sid"] = sid
+        self.br["PIN"] = pin
+        self.br.submit()
 
-    return schedule
+    # Returns student's first name
+    def grab_username(self):
+        # Enter Personal Information section
+        self.br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_GenMenu?name=bmenu.P_GenMnu")
+        # Enter Directory Profile page
+        self.br.open(self.br.click_link(url="/prod_enUS/bwgkoprf.P_ShowDiroItems"))
+        # Use BeautifulSoup to extract info from raw html
+        soup = BeautifulSoup(self.br.response().read(), "lxml")
+        # Extract and return student name from soup
+        return soup.find("td", class_="dedefault").string.split()[0]
 
+    # Returns student's schedule
+    def grab_schedule(self):
+        # Enter Student -> Registration section
+        self.br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu")
+        # Enter Student Detail Schedule page
+        self.br.open(self.br.click_link(url="/prod_enUS/bwskfshd.P_CrseSchdDetl"))
+        # Select current semester to be displayed
+        try:
+            self.br.select_form(nr=1)
+            self.br.submit()
+        except mechanize._mechanize.FormNotFoundError:
+            pass
+        # Use BeautifulSoup to extract info from raw html
+        soup = BeautifulSoup(self.br.response().read(), "lxml")
+        # Initialize schedule dictionary that'll hold course details
+        schedule = {}
+        key = None
+        # Loop through tables with datadisplaytable class
+        for table in soup.find_all("table", class_="datadisplaytable"):
+            # If it's the heading table
+            if table.caption.string != "Scheduled Meeting Times":
+                # Split table caption into three parts ['name', 'number', 'section']
+                caption = table.caption.string.split(" - ")
+                # Store array key as course number after removing spaces
+                key = caption[1].replace(' ', '')
+                # Store all table cells into row array
+                row = table.find_all("td", class_="dddefault")
+                # Store courses info as: schedule[number] = [name, section, CRN, prof_name, prof_email, credit_hours]
+                schedule[key] = [[caption[0], caption[2], row[1].string, row[3].a.get("target"),
+                                 row[3].a.get("href").split(':')[1], row[5].string.split()[0][0]]]
+        # Return to Student -> Registration section
+        self.br.open("https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu")
+        # Enter Student Detail Schedule page
+        self.br.open(self.br.click_link(url="/prod_enUS/uos_dispschd.P_DispCrseSchdSum"))
+        # Use BeautifulSoup to extract info from raw html
+        soup = BeautifulSoup(self.br.response().read(), "lxml")
+        # Loop through TR tags of tables with datadisplaytable class
+        for table in soup.find_all("table", class_="datadisplaytable")[1].find_all("tr"):
+            # Exclude rows with TH tag or that doesn't have valign attribute
+            if table.th is None and table.has_attr("valign"):
+                # Store all table cells into row array
+                row = table.find_all("td", class_="dddefault")
+                # Try and fix if location info is divided into parts
+                try:
+                    # Split and store the location info
+                    location = row[6].string.split()
+                    # If building is repeated in room info
+                    if location[0] in location[1]:
+                        # Store only the actual room info
+                        room = location[1].split('-')[1]
+                    else:
+                        # Otherwise store the room info as is
+                        room = location[1]
+                # If this fails for some reason
+                except Exception:
+                    # Store the whole location string as is
+                    location = [row[6].string]
+                    # Set where room should be that room info is within location
+                    room = "Check in location"
+                # Add more info to schedule as: [[days in chars], [start/end class time], [building, room]]
+                schedule[row[0].string].extend([list(row[4].string), row[5].string.split(" - "), [location[0], room]])
+        # Return the complete student schedule
+        return schedule
--- web.py
+++ web.py
@@ -3,86 +3,86 @@
 # import needed libraries
 import os
 from flask import Flask, render_template, flash, request, redirect, url_for, session
-from info import login, grab_username, grab_schedule
+from info import Info
 from functools import wraps
 
 # initialize with template folder in ./static
-app = Flask(__name__, template_folder='static')
+app = Flask(__name__, template_folder="static")
 # configure security key
-app.secret_key = 'development key'
+app.secret_key = "development key"
+# instantiate an info object
+info = Info()
+
 
 # login required decorator
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if 'sid' in session and 'pin' in session:
+        if "sid" in session:
             return f(*args, **kwargs)
         else:
-            flash('You need to login first!')
-            return redirect(url_for('index'))
+            flash("You need to login first!")
+            return redirect(url_for("index"))
     return wrap
 
+
 # Home page handling
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    # If user is logged in goto dashboard
-    if 'sid' in session and 'pin' in session:
-        return redirect(url_for('dashboard'))
+    # If logged in, flash welcome back & go to dashboard
+    if "sid" in session:
+        flash("Welcome back " + session["name"])
+        return redirect(url_for("dashboard"))
     # Store login info and go to dashboard
     if request.method == "POST":
-        session['sid'] = request.form["sid"]
-        session['pin'] = request.form["pin"]
-        session['post'] = True
-        return redirect(url_for('dashboard'))
-    # If no login, return index page
+        # login and check if it succeeded
+        info.login(request.form["sid"], request.form["pin"])
+        if info.br.title() == "Main Menu":
+            # If logged in, store sid and name in session
+            session["sid"] = request.form["sid"]
+            session["name"] = info.grab_username()
+            # Flash welcoming message and go to dashboard
+            flash("Welcome " + session["name"])
+            return redirect(url_for("dashboard"))
+        else:
+            # Flash an error if login fails
+            flash("Invalid ID or Password")
+    # If no login, return to index page
     return render_template("index.html")
+
 
 # Dashboard page code
 @app.route("/dashboard/")
 @login_required
 def dashboard():
-    # Only if user posted login info
-    if 'post' in session:
-        session.pop('post', None)
-        # login and check if it succeeded
-        br = login(session['sid'], session['pin'])
-        if br.title() == "Main Menu":
-            session['name'] = grab_username(br)
-            flash("Welcome " + session['name'])
-            schedule = grab_schedule(br)
-            return render_template("dashboard.html", data=schedule)
-        else: # If it fails to login
-            # Remove credentials from session
-            session.pop('sid', None)
-            session.pop('pin', None)
-            # Flash an error and go back home
-            flash("Invalid ID or Password")
-            return redirect(url_for('index'))
-    else:
-        flash("Welcome back " + session['name'])
-        return render_template("dashboard.html")
+    # Try to grab student schedule and go to dashboard
+    try:
+        schedule = info.grab_schedule()
+    except Exception:
+        schedule = {"Error:": [["Session Expired!"]]}
+    return render_template("dashboard.html", data=schedule)
 
 
 @app.route("/logout/")
 @login_required
 def logout():
-    flash('See you soon ' + session['name'] + ' ^_^')
-    session.pop('name', None)
-    session.pop('sid', None)
-    session.pop('pin', None)
-    return redirect(url_for('index'))
+    # Flash bye message, delete student info and go home
+    flash("See you soon " + session["name"] + " ^_^")
+    session.pop("name", None)
+    session.pop("sid", None)
+    return redirect(url_for("index"))
 
 
 @app.route("/general/")
 def general():
-    return redirect(url_for('dashboard'))
+    return redirect(url_for("dashboard"))
 
 
 @app.route("/about/")
 def about():
-    return redirect(url_for('dashboard'))
+    return redirect(url_for("dashboard"))
 
 # finalize configurations and run the app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
