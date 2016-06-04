bee8fda5478892519232fcf9e07de680edf4dade
OmarEinea <eineao@gmail.com>
2016-06-05 03:06:35 +0400

    Link SEKRET_KEY and add backgroud
    Remove hard coded key, and link key to os variables.
    
    Now I can make the repo public :P

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Base(object):
     # Disable debugger
     DEBUG = False
     # Define a randomly generated secret key for flask session
-    SECRET_KEY = "\xb4hZ\xf2wc*3\x15(\xc2B\x0e\xf7\xe1n\xbf\xe5_Y\xdfz0\xaa"
+    SECRET_KEY = os.environ["SECRET_KEY"]
     # Define database url according to the current environment
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
     # Disable SQLAlchemy track modifications to avoid warning
